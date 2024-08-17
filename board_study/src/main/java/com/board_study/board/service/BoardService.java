a230a69a566cec9275e482f0b7aeac85898b6dd4
Saru <heo_dark@naver.com>
2024-08-17 16:03:17 +0900

    feat: 삭제 비밀번호 검증, 코드 리팩토링

--- board_study/src/main/java/com/board_study/board/service/BoardService.java
+++ board_study/src/main/java/com/board_study/board/service/BoardService.java
@@ -6,9 +6,9 @@ import com.board_study.board.model.BoardResponse;
 import com.board_study.board.repository.BoardRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -18,6 +18,7 @@ public class BoardService {
     private final BoardRepository boardRepository;
 
     // 글 전체 조회
+    @Transactional(readOnly = true)
     public List<BoardResponse> getAllBoards() {
         return boardRepository.findAll().stream()
                 .filter(board -> "N".equals(board.getDeleteYn()))
@@ -26,57 +27,50 @@ public class BoardService {
     }
 
     // 글 생성
-    public BoardResponse createBoard(BoardRequest request) {
-        Board board = request.toEntity();
+    @Transactional
+    public BoardResponse createBoard(BoardRequest boardRequest) {
+        Board board = boardRequest.toEntity();
         Board savedBoard = boardRepository.save(board);
         return BoardResponse.fromEntity(savedBoard);
     }
 
     // 글 단건 조회 및 조회수 증가
+    @Transactional
     public BoardResponse getBoard(Long id) {
-        Optional<Board> optionalBoard = boardRepository.findById(id);
+        Board board = boardRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("게시글이 존재하지 않습니다. " + id));
 
-        if (optionalBoard.isPresent()) {
-            Board board = optionalBoard.get();
-            // 조회수 증가
-            board.setViewCount(board.getViewCount() + 1);
-            boardRepository.save(board);
-            return BoardResponse.fromEntity(board);
-        }
-
-        throw new RuntimeException("아이디 없어요 " + id);
+        // 조회수 증가
+        board.increaseViewCount();
+        return BoardResponse.fromEntity(board);
     }
 
     // 글 수정
-    public BoardResponse updateBoard(Long id, BoardRequest request) {
-        Optional<Board> optionalBoard = boardRepository.findById(id);
-        if (optionalBoard.isPresent()) {
-            Board board = optionalBoard.get();
-            // 비밀번호 확인
-            if (board.getPassword().equals(request.password())) {
-                board.setTitle(request.title());
-                board.setContent(request.content());
-                board.setAuthor(request.author());
-                boardRepository.save(board);
-                return BoardResponse.fromEntity(board);
-            } else {
-                throw new RuntimeException("비번틀림.");
-            }
-        }
-        // 예외 처리
-        throw new RuntimeException("아이디 없어요: " + id);
+    @Transactional
+    public BoardResponse updateBoard(Long id, BoardRequest boardRequest) {
+        Board board = boardRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("게시글이 존재하지 않습니다. " + id));
+
+        validateBoard(board, boardRequest.password());
+        board.update(boardRequest.author(), boardRequest.title(), boardRequest.password());
+
+        return BoardResponse.fromEntity(board);
     }
 
     // 글 삭제 (deleteYn을 "Y"로 변경)
-    public void deleteBoard(Long id) {
-        Optional<Board> optionalBoard = boardRepository.findById(id);
-        if (optionalBoard.isPresent()) {
-            Board board = optionalBoard.get();
-            board.setDeleteYn("Y");
-            boardRepository.save(board);
-        } else {
-            // 예외 처리
-            throw new RuntimeException("아이디 없어요: " + id);
+    @Transactional
+    public void deleteBoard(Long id, String password) {
+        Board board = boardRepository.findById(id)
+                        .orElseThrow(() -> new RuntimeException("게시글이 존재하지 않습니다. " + id));
+
+        validateBoard(board, password);
+        board.updateDelete();
+    }
+
+    // 비밀번호 검증
+    private void validateBoard(Board board, String password) {
+        if (!board.getPassword().equals(password)) {
+            throw new RuntimeException("비밀번호가 틀립니다.");
         }
     }
 }
